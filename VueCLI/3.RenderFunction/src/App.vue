37c29af6815a23c8c2e194c66d33b52318bd2812
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-25 09:21:40 +0100

    Add Course Module: Render Function

---
+++ VueCLI/3.RenderFunction/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <custom-element type="footer">Hola footer</custom-element>
+        <custom-element type="header">Hola header</custom-element>
+        <custom-element type="aside">Hola aside</custom-element>
+        <custom-element type="nav">Hola nav</custom-element>
+        <custom-element type="main">Hola main</custom-element>
+    </div>
+</template>
+
+<script>
+    import customElement from './components/CustomElement.component.vue';
+    export default {
+        components: { customElement }
+    }
+</script>
+
+<style>
+</style>
