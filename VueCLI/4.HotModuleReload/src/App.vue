1df3982b06e61ab1c1f7cdf5ba8dce3f7219e4df
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-25 09:38:22 +0100

    Add Course Module: Hot Module Reload

---
+++ VueCLI/4.HotModuleReload/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <tasks></tasks>
+  </div>
+</template>
+
+<script>
+import tasks from './components/Tasks.component';
+export default {
+  components: {
+    tasks
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ VueCLI/4.HotModuleReload/src/components/Tasks.component.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2>Task for done</h2>
+        <ul>
+            <li style="cursor:pointer" v-for="task in unfinishedTasks" :key="task.name" @click="task.completed = true">{{ task.name}}</li>
+        </ul>
+        <hr>
+        <h2>Task finished</h2>
+        <ul>
+            <li style="cursor:pointer" v-for="task in finishedTasks" :key="task.name" @click="task.completed = false">{{ task.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            tasks: [
+                {name: 'Learn Vuejs', completed: false},
+                {name: 'Rec Vuejs moduel about vuex', completed: false},
+                {name: 'Response comments', completed: false},
+                {name: 'Design firebase module', completed: false}
+            ]
+        }
+    },
+    computed: {
+        unfinishedTasks(){
+            return this.tasks.filter((task) => !task.completed);
+        },
+        finishedTasks(){
+            return this.tasks.filter((task) => task.completed);
+        }
+    }
+}
+</script>
+
+<style>
+li {
+    color: darkgoldenrod;
+    font-weight: bold;
+    text-decoration: none;
+}
+</style>
