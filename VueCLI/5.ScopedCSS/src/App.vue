b328d8b46e6f3e37074458114a76fabff3a9178a
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-26 15:53:28 +0100

    Add Course Module: Scoped CSS

---
+++ VueCLI/5.ScopedCSS/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Principal Menu</h1>
+    <ul>
+      <li><a href="">Link 1</a></li>
+      <li><a href="">Link 2</a></li>
+      <li><a href="">Link 3</a></li>
+      <li><a href="">Link 4</a></li>
+      <li><a href="">Link 5</a></li>
+      <li><a href="">Link 6</a></li>
+    </ul>
+    <hr>
+    <tasks></tasks>
+  </div>
+</template>
+
+<script>
+import tasks from './components/Tasks.component';
+export default {
+  components: {
+    tasks
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VueCLI/5.ScopedCSS/src/components/Tasks.component.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h2>Task for done</h2>
+        <ul>
+            <li style="cursor:pointer" v-for="task in unfinishedTasks" :key="task.name"><a @click="task.completed = true">{{ task.name}}</a></li>
+        </ul>
+        <hr>
+        <h2>Task finished</h2>
+        <ul>
+            <li style="cursor:pointer" v-for="task in finishedTasks" :key="task.name"><a @click="task.completed = false">{{ task.name}}</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            tasks: [
+                {name: 'Learn Vuejs', completed: false},
+                {name: 'Rec Vuejs moduel about vuex', completed: false},
+                {name: 'Response comments', completed: false},
+                {name: 'Design firebase module', completed: false}
+            ]
+        }
+    },
+    computed: {
+        unfinishedTasks(){
+            return this.tasks.filter((task) => !task.completed);
+        },
+        finishedTasks(){
+            return this.tasks.filter((task) => task.completed);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    li a {
+        color: red;
+    }
+</style>
