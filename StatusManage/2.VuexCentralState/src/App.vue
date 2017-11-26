2d9f35d8676cc74a0c82758398c5ada4d4254a5f
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-26 16:47:15 +0100

    Add Course Module: Vuex Central State

---
+++ StatusManage/2.VuexCentralState/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <counter :counter="counter"
+            @increase="counter++"
+            @decrease="counter--">
+  </counter> -->
+  <counter></counter>
+</template>
+
+<script>
+import counter from './components/Counter.component';
+
+export default {
+  components: {
+    counter
+  },
+  // data(){
+  //   return {
+  //     counter: 0
+  //   };  
+  // }
+}
+</script>
+
+<style>
+</style>
---
+++ StatusManage/2.VuexCentralState/src/components/Counter.component.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="increase">Increase</button>
+        <button @click="decrease">Decrease</button>
+    </div>
+</template>
+<script>
+export default {
+    //props: ['counter'],
+    methods: {
+        increase(){
+            //this.$emit('increase');
+            this.$store.state.counter++;
+        },
+        decrease(){
+            //this.$emit('decrease');
+            this.$store.state.counter--;
+        }
+    },
+    computed: {
+        counter(){
+            return this.$store.state.counter;
+        }
+    }
+}
+</script>
+
