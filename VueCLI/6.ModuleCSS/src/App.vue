17f4101e902991004f5e326051f28bc57f3bdb72
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-26 16:12:40 +0100

    Add Course Module: CSS Modules

---
+++ VueCLI/6.ModuleCSS/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1 :class="[$style.red]">Hi CSS Modules!!!</h1>
+  <h1 :class="{[$style.highlight]: highlight}" @click="highlight = !highlight">Hi CSS Modules!!! Part 2</h1>
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      highlight: false
+    };
+  }
+}
+</script>
+
+<style module>
+  .highlight {
+    text-decoration: underline;
+  }
+
+  .red {
+    color: red;
+  }
+</style>
