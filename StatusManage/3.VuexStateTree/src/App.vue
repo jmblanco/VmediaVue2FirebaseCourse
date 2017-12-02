138012207023353568888592e1512ea62364f4f4
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-12-02 07:46:01 +0100

    Add Course Module: Vuex State Tree

---
+++ StatusManage/3.VuexStateTree/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hi my name is: {{ name }} {{ last }} and I work as {{ profession }}</h1>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  // computed: {
+  //   name() {
+  //     return this.$store.state.name;
+  //   }
+  // }
+  // computed: mapState({
+  //   name: (state) => state.name,
+  //   last: (state) => state.last
+  // })
+  computed: mapState(['name', 'last', 'profession', 'city'])
+}
+</script>
